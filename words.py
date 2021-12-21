f8498d85e90bc3c95bcd133663e1f50579863974
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2021-12-21 15:57:52 -0500

    part 3

--- words.py
+++ words.py
@@ -14,3 +14,13 @@ def print_upper_words(words_list):
 
 #Part 3
 
+def print_upper_e_words(words_list):
+    """
+    Given a list of words print each word that starts with 'e'.
+    The words printed must be converted to UPPERCASE letters!
+    Ex:    print_upper_words(["elephant", "wow"])
+            ELEPHANT
+    """
+    for word in words_list:
+        if word.startswith('e'):
+            print(word.upper())
