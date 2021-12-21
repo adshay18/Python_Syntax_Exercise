27d3608a46dea3ca8f76623f428870746c27d996
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2021-12-21 15:54:07 -0500

    part 1 & 2

---
+++ words.py
@@ -0,0 +1,16 @@
+#Part 1 & 2
+
+def print_upper_words(words_list):
+    """
+    Given a list of words print each word.
+    The words printed must be converted to UPPERCASE letters!
+    Ex:    print_upper_words(["hi", "wow"])
+            HI
+            WOW
+    """
+
+    for word in words_list:
+        print(word.upper())
+
+#Part 3
+
